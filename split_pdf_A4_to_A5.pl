899591f7a0744ef92ecff0da23a582b188006c84
Juergen Hackl <hackl@ibi.baug.ethz.ch>
2017-07-28 19:41:29 +0200

    add week view

---
+++ split_pdf_A4_to_A5.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/env perl
+use strict; use warnings;
+use PDF::API2;
+
+my $filename = shift || 'test.pdf';
+my $oldpdf = PDF::API2->open($filename);
+my $newpdf = PDF::API2->new;
+
+for my $page_nb (1..$oldpdf->pages) {
+my ($page, @cropdata);
+
+$page = $newpdf->importpage($oldpdf, $page_nb);
+@cropdata = $page->get_mediabox;
+$cropdata[2] /= 2;
+$page->cropbox(@cropdata);
+$page->trimbox(@cropdata);
+$page->mediabox(@cropdata);
+
+$page = $newpdf->importpage($oldpdf, $page_nb);
+@cropdata = $page->get_mediabox;
+$cropdata[0] = $cropdata[2] / 2;
+$page->cropbox(@cropdata);
+$page->trimbox(@cropdata);
+$page->mediabox(@cropdata);
+}
+
+(my $newfilename = $filename) =~ s/(.*)\.(\w+)$/$1.split.$2/;
+$newpdf->saveas($newfilename);
+
+__END__
